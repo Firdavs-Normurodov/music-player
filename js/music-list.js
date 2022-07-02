ecdadbb40bba80e8f6b6dac3d21a921dbdc2f503
Firdavs-Normurodov <firdavsnormurodov435@gmail.com>
2022-07-02 21:24:46 +0500

    test

---
+++ js/music-list.js
@@ -0,0 +1,51 @@
+
+let allMusic = [
+  {
+    name: "Right Here Waiting",
+    artist: "Richard Marx",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Faded & Alan Walker",
+    artist: "Alan Walker",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Indila & Love Story",
+    artist: "Indila - Love Story",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Fly Project & Toca-toca",
+    artist: "Fly Project",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tones And I",
+    artist: "Dance Monkey",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Love Me LIke You Do",
+    artist: "AMV ML",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name:"Let Me Down Slowly Lyrics",
+    artist:"Alec Benjamin",
+    img:"music-7",
+    src:"music-7"
+  },
+  {
+    name:"Skip The Use ",
+    artist:"Nameless World",
+    img:"music-8",
+    src:"music-8",
+  } 
+];
